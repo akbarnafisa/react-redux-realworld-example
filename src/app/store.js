e957d8bcdfff808ec15cff80aef4a5ce5d6faa60
Akbar Nafisa <akbarnj788@gmail.com>
2023-08-09 17:35:44 +0700

    feat: init store

---
+++ src/app/store.js
@@ -0,0 +1,28 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import { connectRouter, routerMiddleware } from 'connected-react-router';
+
+// import authReducer from '../features/auth/authSlice';
+// import commentsReducer from '../features/comments/commentsSlice';
+import tagsReducer from '../features/tags/tagsSlice';
+
+// import history from './history';
+// import { localStorageMiddleware } from './middleware';
+// import articleReducer from '../reducers/article';
+import articlesReducer from '../reducers/articleList';
+// import commonReducer from '../reducers/common';
+// import profileReducer from '../reducers/profile';
+
+export function makeStore (preloadedState) {
+  return configureStore({
+    reducer: {
+      articleList: articlesReducer,
+      tags: tagsReducer,
+    },
+    devTools: true,
+    preloadedState,
+  })
+}
+
+const store = makeStore();
+
+export default store;
---
+++ src/reducers/articleList.js
@@ -0,0 +1,107 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import agent from '../agent';
+// import { profilePageUnloaded } from './profile';
+
+export const changeTab = (tab) => (dispatch) => {
+  dispatch(articleListSlice.actions.changeTab(tab));
+  return dispatch(getAllArticles());
+};
+
+export const getAllArticles = createAsyncThunk(
+  'articleList/getAll',
+  ({ page, author, tag, favorited } = {}, thunkApi) =>
+    thunkApi.getState().articleList.tab === 'feed'
+      ? agent.Articles.feed(page)
+      : agent.Articles.all({
+          page: page ?? thunkApi.getState().articleList.currentPage,
+          author: author ?? thunkApi.getState().articleList.author,
+          tag: tag ?? thunkApi.getState().articleList.tag,
+          favorited: favorited ?? thunkApi.getState().articleList.favorited,
+          limit: thunkApi.getState().articleList.articlesPerPage ?? 10,
+        })
+);
+
+export const getArticlesByTag = createAsyncThunk(
+  'articleList/getArticlesByTag',
+  ({ tag, page } = {}) => agent.Articles.byTag(tag, page)
+);
+
+export const favoriteArticle = createAsyncThunk(
+  'articleList/favoriteArticle',
+  (slug) => agent.Articles.favorite(slug)
+);
+
+export const unfavoriteArticle = createAsyncThunk(
+  'articleList/unfavoriteArticle',
+  (slug) => agent.Articles.unfavorite(slug)
+);
+
+const initialState = {
+  articles: [],
+  articlesCount: 0,
+  currentPage: 0,
+  articlesPerPage: 10,
+  tab: undefined,
+  tag: undefined,
+  author: undefined,
+  favorited: undefined,
+};
+
+const articleListSlice = createSlice({
+  name: 'articleList',
+  initialState,
+  reducers: {
+    homePageUnloaded: () => initialState,
+    changeTab: (state, action) => {
+      state.tab = action.payload;
+      delete state.tag;
+    },
+  },
+
+  extraReducers: (builder) => {
+
+    builder.addCase(favoriteArticle.fulfilled, (state, action) => {
+      state.articles = state.articles.map((article) =>
+        article.slug === action.payload.article.slug
+          ? {
+              ...article,
+              favorited: action.payload.article.favorited,
+              favoritesCount: action.payload.article.favoritesCount,
+            }
+          : article
+      );
+    });
+
+
+    builder.addCase(unfavoriteArticle.fulfilled, (state, action) => {
+      state.articles = state.articles.map((article) =>
+        article.slug === action.payload.article.slug
+          ? {
+              ...article,
+              favorited: action.payload.article.favorited,
+              favoritesCount: action.payload.article.favoritesCount,
+            }
+          : article
+      );
+    });
+
+    builder.addCase(getAllArticles.fulfilled, (state, action) => {
+      state.articles = action.payload.articles;
+      state.articlesCount = action.payload.articlesCount;
+      state.currentPage = action.meta.arg?.page ?? 0;
+    });
+
+    builder.addCase(getArticlesByTag.fulfilled, (state, action) => ({
+      articles: action.payload.articles,
+      articlesCount: action.payload.articlesCount,
+      currentPage: action.meta.arg?.page ?? 0,
+      tag: action.meta.arg?.tag,
+      articlesPerPage: 10,
+    }));
+  },
+});
+
+export const { homePageUnloaded } = articleListSlice.actions;
+
+export default articleListSlice.reducer;
