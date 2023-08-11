12b96946ce8ac4b3cf598d6d746229f49fa69c1f
Akbar Nafisa <akbarnj788@gmail.com>
2023-08-11 15:58:41 +0700

    feat: add your feed tab

--- src/components/Home/MainView.js
+++ src/components/Home/MainView.js
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ArticleList from '../ArticleList';
 import { changeTab } from '../../reducers/articleList';
+import { selectIsAuthenticated } from '../../features/auth/authSlice';
 
 /**
  * Global feed tab
@@ -31,6 +32,40 @@ function GlobalFeedTab() {
   );
 }
 
+/**
+ * Your feed tab
+ *
+ * @example
+ * <YourFeedTab />
+ */
+function YourFeedTab() {
+  const dispatch = useDispatch()
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  const currentTab = useSelector((state) => state.articleList.tab)
+  const isActiveTab = currentTab === 'feed';
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
+  const dispatchChangeTab = () => {
+    dispatch(changeTab('feed'));
+  };
+
+  return (
+    <li className="nav-item">
+      <button
+        type="button"
+        className={isActiveTab ? 'nav-link active' : 'nav-link'}
+        onClick={dispatchChangeTab}
+      >
+        Your Feed
+      </button>
+    </li>
+  );
+
+}
+
 function TagFilterTab() {
   const tag = useSelector((state) => state.articleList.tag)
 
@@ -52,10 +87,8 @@ function MainView() {
     <div className="col-md-9">
       <div className="feed-toggle">
         <ul className="nav nav-pills outline-active">
-          {/* <YourFeedTab /> */}
-
+        <YourFeedTab />
           <GlobalFeedTab />
-
           <TagFilterTab />
         </ul>
       </div>
